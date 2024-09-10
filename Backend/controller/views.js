316a01f619eeca353ba412b4254f02c6a257c8c6
andrwe <andrwe455@ggmail.com>
2024-09-10 16:53:27 -0500

    update control views
    this is an control views update

--- Backend/controller/views.js
+++ Backend/controller/views.js
@@ -9,7 +9,25 @@ function secondFactor (req, res) {
     res.sendFile(path.join(proyectPath,'frontend','secondFactor.html'));
 }
 
+function home (req, res) {
+    const proyectPath = path.resolve(__dirname, '../..');
+
+    console.log(req);
+    if(req.params.role === 'admin'){
+        console.log('admin');
+        res.sendFile(path.join(proyectPath,'Frontend','views','modules','adminDashboard','adminDashboardHome.html'));
+        
+    }else if( req.body.role === 'teacher'){
+        res.sendFile(path.join(proyectPath,'frontend','modules','teachersDashboard','teacher.html'));
+    }else if( req.body.role === 'student'){
+        res.sendFile(path.join(proyectPath,'frontend','modules','studentsDashboard','student.html'));
+    }else{
+        res.json({message: 'rol not found'});
+    }
+}
+
 module.exports = {
     login,
-    secondFactor
+    secondFactor,
+    home
 }
