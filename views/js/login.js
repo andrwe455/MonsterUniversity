bdc73f92ee04e0ff96b8194882ff687bb76a6167
juan_castro82211 <juan_castro@elpoli.edu.co>
2024-09-12 19:44:04 -0600

    Updated navigation bar
    Deleted unnecesary icons, left only the useful ones and updated the notifications overlay

--- views/js/login.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    
-    const urlParams = new URLSearchParams(window.location.search);
-    const errorMessage = urlParams.get('error');
-    if (errorMessage) {
-        document.getElementById('error-message').style.display = 'block';
-    }
-  
-}
