37b7f1f24726c35cdfc50c443d58d9c31f2fe7e8
juan_castro82211 <juan_castro@elpoli.edu.co>
2024-09-22 12:30:34 -0600

    Add teachers evaluation review
    Added the teachers evaluation review for the manager with the feature of removing or sending a warning to the respective teachers

--- Frontend/views/js/todolist.js
+++ Frontend/views/js/todolist.js
@@ -1,75 +1,78 @@
 document.getElementById("showInputBtn").addEventListener("click", function() {
-    document.getElementById("inputModal").style.display = "flex";
+  document.getElementById("inputModal").style.display = "flex";
 });
 
 document.getElementById("closeModal").addEventListener("click", function() {
-    document.getElementById("inputModal").style.display = "none";
+  document.getElementById("inputModal").style.display = "none";
 });
 
 function deleteElement(element) {
-    var listItem = element.closest("li");
-    var list = listItem.parentElement; 
+  var listItem = element.closest("li");
+  var list = listItem.parentElement; 
     
-    if (listItem) {
-        listItem.remove(); 
-    }
+  if (listItem) {
+    listItem.remove(); 
+  }
 
-    var remainingItems = list.getElementsByTagName("li").length;
+  var remainingItems = list.getElementsByTagName("li").length;
 
-    if (remainingItems === 0) {
-        document.getElementById("toDoPlaceholder").style.display = "flex";
-    } 
+  if (remainingItems === 0) {
+    document.getElementById("toDoPlaceholder").style.display = "flex";
+  } 
 }
 
 function addItem() {
-    var inputValue = document.getElementById("inputField").value;
+  var inputValue = document.getElementById("inputField").value;
 
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
+  if (inputValue === '') {
+    alert("You must write something!");
+  }
+  else {
 
-        var li = document.createElement("li");
-        document.getElementById("toDoPlaceholder").style.display = "none";
+    var li = document.createElement("li");
+    document.getElementById("toDoPlaceholder").style.display = "none";
 
-        li.innerHTML = `
-            <span class="handle">
-              <i class="fas fa-ellipsis-v"></i>
-              <i class="fas fa-ellipsis-v"></i>
-            </span>
-            <div class="icheck-primary d-inline ml-2">
-              <input type="checkbox" value="" name="todo${Date.now()}" id="todoCheck${Date.now()}">
-              <label for="todoCheck${Date.now()}"></label>
-            </div>
-            <span class="text">${inputValue}</span>
-            <div class="tools">
-              <i class="fas fa-trash delete-icon"></i>
-            </div>
-        `;
+    li.innerHTML = `
+      <span class="handle">
+        <i class="fas fa-ellipsis-v"></i>
+        <i class="fas fa-ellipsis-v"></i>
+      </span>
 
-        document.getElementById("listUL").appendChild(li);
-        document.getElementById("inputModal").style.display = "none";
-    }
+      <div class="icheck-primary d-inline ml-2">
+        <input type="checkbox" value="" name="todo${Date.now()}" id="todoCheck${Date.now()}">
+        <label for="todoCheck${Date.now()}"></label>
+      </div>
+      
+      <span class="text">${inputValue}</span>
+      <div class="tools">
+        <i class="fas fa-trash delete-icon"></i>
+      </div>
+    `;
 
-    document.getElementById("inputField").value = "";
-    const trashIcon = li.querySelector('.delete-icon');
+    document.getElementById("listUL").appendChild(li);
+    document.getElementById("inputModal").style.display = "none";
+  }
 
-    trashIcon.addEventListener('click', function() {
-        deleteElement(this);
-    });
+  document.getElementById("inputField").value = "";
+  const trashIcon = li.querySelector('.delete-icon');
+
+  trashIcon.addEventListener('click', function() {
+    deleteElement(this);
+  });
 }
 
 document.getElementById("confirmBtn").addEventListener("click", function() {
-    addItem();
+  addItem();
 });
 
 document.getElementById("inputField").addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {  
-        addItem(); 
-    }
+  if (event.key === "Enter") {  
+    addItem(); 
+  }
 });
 
 window.addEventListener("click", function(event) {
-    if (event.target == document.getElementById("inputModal")) {
-        document.getElementById("inputModal").style.display = "none";
-    }
+  if (event.target == document.getElementById("inputModal")) {
+    document.getElementById("inputModal").style.display = "none";
+  }
 });
