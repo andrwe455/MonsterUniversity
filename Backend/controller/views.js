836c31c80cc4f6f5d242202b288f65854bdcfeb4
Mariana <mariana_penagos04192@elpoli.edu.co>
2024-09-26 17:09:32 -0500

    edit files
     pdate student dashboard views and routes
    
    - Updated HTML files in  module:
      -
      -
      -
      -
      -
    
    - Created new folders for better organization.
    - Added new functions to views and updated routes for:
      - Academic history
      - Attendance
      - Inscriptions
      - Teacher evaluation

---
+++ Backend/controller/views.js
@@ -0,0 +1,67 @@
+const path = require('path');
+
+function login (req, res) {
+    res.redirect('/');
+}
+
+function secondFactor (req, res) {
+    const proyectPath = path.resolve(__dirname, '../..');
+    res.sendFile(path.join(proyectPath,'frontend','secondFactor.html'));
+}
+
+function home (req, res) {
+    const proyectPath = path.resolve(__dirname, '../..');
+    console.log(req.params.role);
+    if(req.params.role === 'admin'){
+        res.sendFile(path.join(proyectPath,'Frontend','views','modules','adminDashboard','adminDashboardHome.html'));  
+    }else if( req.params.role === 'teacher'){
+        res.sendFile(path.join(proyectPath,'Frontend','views','modules','teachersDashboard','teacher.html'));
+    }else if( req.params.role=== 'student'){
+        res.sendFile(path.join(proyectPath,'Frontend','views','modules','dashboard_student','Home.html'));
+    }
+    else if( req.params.role=== 'manager'){
+        res.sendFile(path.join(proyectPath,'Frontend','views','modules','managerDashboard','managerDashboardHome.html'));
+    }
+    else{
+        res.json({message: 'rol not found'});
+    }
+}
+
+function createSubject (req, res) {
+    const proyectPath = path.resolve(__dirname, '../..');
+    res.sendFile (path.join(proyectPath,'Frontend','views','modules','adminDashboard','subjects','adminDashboarCreateSubject.html'));
+}
+
+
+function createStudentAcademicHistory(req, res) {
+    const proyectPath = path.resolve(__dirname, '../..');
+    res.sendFile(path.join(proyectPath, 'Frontend', 'views', 'modules', 'dashboard_student','studentAcademicHistory' ,'studentAcademicHistory.html'));
+}
+
+function createStudentTeachersEvaluation(req, res) {
+    const proyectPath = path.resolve(__dirname, '../..');
+    res.sendFile(path.join(proyectPath, 'Frontend', 'views', 'modules', 'dashboard_student', 'StudentTeachersEvaluation', 'StudentTeachersEvaluation.html'));
+}
+
+function createStudentAttendancePerSubject(req, res) {
+    const proyectPath = path.resolve(__dirname, '../..');
+    res.sendFile(path.join(proyectPath, 'Frontend', 'views', 'modules', 'dashboard_student','StudentAttendancePerSubject' ,'StudentAttendancePerSubject.html'));
+}
+
+function createStudentInscripcions(req, res) {
+    const proyectPath = path.resolve(__dirname, '../..');
+    res.sendFile(path.join(proyectPath, 'Frontend', 'views', 'modules', 'dashboard_student','Studentinscripcions' ,'StudentInscripcions.html'));
+}
+
+
+module.exports = {
+    login,
+    secondFactor,
+    home,
+    createSubject,
+    createStudentAcademicHistory,
+    createStudentTeachersEvaluation,
+    createStudentAttendancePerSubject,
+    createStudentInscripcions
+    
+}
--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,10 +1,33 @@
 const {Router} = require('express');
 const router = Router();
 const controller = require('../controller/controller.js');
+const viwes = require('../controller/views.js');
 
-const mongoose = require('../database/mongo.js');
+
+//pages routes
+
+/**
+ * 
+ */
+router.get('/login', viwes.login);
+router.get('/secondFactor', viwes.secondFactor);
+router.get('/home/:role', viwes.home);
+
+//admin routes
+router.get('/home/admin/createSubject', viwes.createSubject);
 
 
+//Student routes
+router.get('/home/student/AcedmicHistory', viwes.createStudentAcademicHistory);
+router.get('/home/student/TeachersEvaluation', viwes.createStudentTeachersEvaluation);
+router.get('/home/student/AttendancePerSubject', viwes.createStudentAttendancePerSubject);
+router.get('/home/student/Inscripcions', viwes.createStudentInscripcions);
+
+//api routes
+
+router.get('/logout', controller.Logout);
+
+router.post('/login', controller.login);
 router.post('/setTeacher', controller.setTeacher);
 router.post('/scoreTeacher/:id', controller.teacherScore);
 
