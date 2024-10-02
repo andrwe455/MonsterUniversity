d88169fb4841af25562be72e1e3671ed7a6a15e3
nombre15 <juanseflash8@gmail.com>
2024-10-01 23:58:13 -0500

    Feat notifications
    Feature for working notifications with numbers retrieved from the database

---
+++ Frontend/views/js/modules/manager/notification.js
@@ -0,0 +1,24 @@
+fetch('/getInfo').then(res => res.json()).then(data => {
+  let evaluations = 0;
+  let answeredEvaluations = 0;
+
+  if(data.teachers.length > 0){
+    let teachers = data.teachers;
+
+    teachers.forEach(teacher => {
+      evaluations += teacher.test.length;
+      teacher.test.forEach(semester => {
+        if(semester.status){
+          answeredEvaluations++;
+        }
+      });
+    });
+  }
+  const pendingEvaluations = evaluations - answeredEvaluations;
+  const pendingElements = document.querySelectorAll('.pending'); 
+
+  pendingElements.forEach((pendingElement) => {
+    pendingElement.innerHTML = pendingEvaluations; 
+  });
+});
+  
